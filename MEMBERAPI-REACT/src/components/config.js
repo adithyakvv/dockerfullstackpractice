f70b84764f1319bdfb17e4f6bc03beb6061c4c53
KVV <2300033045@kluniversity.in>
2025-09-25 11:30:50 +0530

    updated

--- MEMBERAPI-REACT/src/components/config.js
+++ MEMBERAPI-REACT/src/components/config.js
@@ -1,6 +1,6 @@
 // This configuration file exports the base URL of our backend API.
 const config = {
-    url: "http://localhost:2030/MEMBERAPI-SPRINGBOOT"
+    url: "http://localhost:2030/memberapi-springboot"
 };
 
 export default config;
